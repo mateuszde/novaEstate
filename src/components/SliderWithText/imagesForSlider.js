a5d7607af0f0ca20aceb3115aa4e1a5a85dc5902
Mateusz <mat.demski@gmail.com>
2022-11-15 01:45:44 +0100

    images have been added to slider gallery

--- src/components/SliderWithText/imagesForSlider.js
+++ src/components/SliderWithText/imagesForSlider.js
@@ -2,7 +2,14 @@ const relativePath = `${process.env.PUBLIC_URL}`;
 
 const imagesPath = {
   0: `${relativePath}/img/headerBgc1.jpg`,
-  1: `${relativePath}/img/textOnImage1.jpg`,
+  1: `${relativePath}/img/2.JPG`,
+  2: `${relativePath}/img/3.JPG`,
+  3: `${relativePath}/img/4.JPG`,
+  4: `${relativePath}/img/5.JPG`,
+  5: `${relativePath}/img/6.JPG`,
+  6: `${relativePath}/img/7.JPG`,
+  7: `${relativePath}/img/8.JPG`,
+  8: `${relativePath}/img/9.JPG`,
 };
 
 const addImagesToArr = (source, target) => {
